9f49bdd3e57678f4ed1f388b6faabe2914273f98
Marielen Rezende <marhih.almeida@gmail.com>
2022-09-16 11:28:50 -0300

    adicionado lista de palavras do jogo

---
+++ js/script.js
@@ -0,0 +1,82 @@
+const palavras = [
+  palavra001 = {
+    nome = 'IRLANDA',
+    categoria = 'LUGARES'
+  },
+  palavra002 = {
+    nome = 'VENEZUELA',
+    categoria = 'LUGARES'
+  },
+  palavra003 = {
+    nome = 'BRASIL',
+    categoria = 'LUGARES'
+  },
+  palavra004 = {
+    nome = 'ARGENTINA',
+    categoria = 'LUGARES'
+  },
+  palavra005 = {
+    nome = 'RUSSIA',
+    categoria = 'LUGARES'
+  },
+  palavra006 = {
+    nome = 'CANADA',
+    categoria = 'LUGARES'
+  },
+  palavra007 = {
+    nome = 'LASANHA',
+    categoria = 'COMIDA'
+  },
+  palavra008 = {
+    nome = 'FRANGO',
+    categoria = 'COMIDA'
+  },
+  palavra009 = {
+    nome = 'CHURRASCO',
+    categoria = 'COMIDA'
+  },
+  palavra010 = {
+    nome = 'ARROZ',
+    categoria = 'COMIDA'
+  },
+  palavra011 = {
+    nome = 'SALADA',
+    categoria = 'COMIDA'
+  },
+  palavra012 = {
+    nome = 'PIZZA',
+    categoria = 'COMIDA'
+  },
+  palavra012 = {
+    nome = 'PIZZA',
+    categoria = 'COMIDA'
+  },
+  palavra013 = {
+    nome = 'NAVIO',
+    categoria = 'TRANSPORTE'
+  },
+  palavra014 = {
+    nome = 'CARRO',
+    categoria = 'TRANSPORTE'
+  },
+  palavra015 = {
+    nome = 'BICICLETA',
+    categoria = 'TRANSPORTE'
+  },
+  palavra016 = {
+    nome = 'ONIBUS',
+    categoria = 'TRANSPORTE'
+  },
+  palavra017 = {
+    nome = 'AVIAO',
+    categoria = 'TRANSPORTE'
+  },
+  palavra018 = {
+    nome = 'MOTO',
+    categoria = 'TRANSPORTE'
+  }
+];
+
+function criarPalavraSecreta{
+  
+}
